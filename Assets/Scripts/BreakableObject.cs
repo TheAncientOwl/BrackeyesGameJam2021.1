dc7ad47831e8749a3c7315d3af0eb16a493f3aa2
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-02-22 23:41:09 +0200

    meh

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -5,7 +5,7 @@ public class BreakableObject : MonoBehaviour
     private bool m_Alive = true;
     public void Break()
     {
-        Destroy(this.gameObject, 5f);
+        Destroy(this.gameObject, 2f);
         Debug.Log("IGHT IMMA HEAD OUT SOON LOL. xD");
         m_Alive = false;
     }
